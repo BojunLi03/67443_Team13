182cfcae80e0ecd1bccd96cb9d5b8922b2abd31a
Your Name <your@email.address>
2024-10-16 14:50:41 -0500

    Initial commit for Xcode project

---
+++ GameOn/GameOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOnApp.swift
+//  GameOn
+//
+//  Created by User on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GameOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
